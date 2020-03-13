28f3e376bdc2fc766916d237c4855ca73167c1df
unknown <ch4@illinois.edu>
2020-03-13 15:53:19 -0500

    Saving led_strip_test file

--- led_strip_test/led_strip_test.ino
+++ led_strip_test/led_strip_test.ino
@@ -71,23 +71,35 @@ void setup() {
     }
   }
   
-  for (int i = ROW0; i < 41; i+=3) {
+  for (int i = 38; i >= ROW0; i-=3) {
+    delay(300);
     strip.fill(strip.Color(red[0][(i-ROW0)/3], blue[0][(i-ROW0)/3], green[0][(i-ROW0)/3]), i, 2); // Stops after index 38
+    strip.show();
   }
   for (int j = ROW1; j < 89; j+=3) {
+    delay(300);
     strip.fill(strip.Color(red[1][(j-ROW1)/3], blue[1][(j-ROW1)/3], green[1][(j-ROW1)/3]), j, 2); // Stops after index 86
+    strip.show();
   }
-  for (int k = ROW2; k < 139; k+=3) {
+  for (int k = 136; k >= ROW2; k-=3) {
+    delay(300);
     strip.fill(strip.Color(red[2][(k-ROW2)/3], blue[2][(k-ROW2)/3], green[2][(k-ROW2)/3]), k, 2); // Stops after index 136
+    strip.show();
   }
   for (int l = ROW3; l < 188; l+=3) {
+    delay(300);
     strip.fill(strip.Color(red[3][(l-ROW3)/3], blue[3][(l-ROW3)/3], green[3][(l-ROW3)/3]), l, 2); // Stops after index 186
+    strip.show();
   }
-  for (int m = ROW4; m < 238; m+=3) {
+  for (int m = 236; m >= ROW4; m-=3) {
+    delay(300);
     strip.fill(strip.Color(red[4][(m-ROW4)/3], blue[4][(m-ROW4)/3], green[4][(m-ROW4)/3]), m, 2); // Stops after index 236
+    strip.show();
   }
   for (int n = ROW5; n < 288; n+=3) {
+    delay(300);
     strip.fill(strip.Color(red[5][(n-ROW5)/3], blue[5][(n-ROW5)/3], green[5][(n-ROW5)/3]), n, 2); // Stops after index 286
+    strip.show();
   }
   strip.show(); // Initialize all specific pixels to 'on'
 }
@@ -112,19 +124,19 @@ void loop() {
     green[5][x] = prev_green;
   }
 
-  for (int i = ROW0; i < 41; i+=3) {
+  for (int i = 38; i >= ROW0; i-=3) {
     strip.fill(strip.Color(red[0][(i-ROW0)/3], blue[0][(i-ROW0)/3], green[0][(i-ROW0)/3]), i, 2); // Stops after index 38
   }
   for (int j = ROW1; j < 89; j+=3) {
     strip.fill(strip.Color(red[1][(j-ROW1)/3], blue[1][(j-ROW1)/3], green[1][(j-ROW1)/3]), j, 2); // Stops after index 86
   }
-  for (int k = ROW2; k < 139; k+=3) {
+  for (int k = 136; k >= ROW2; k-=3) {
     strip.fill(strip.Color(red[2][(k-ROW2)/3], blue[2][(k-ROW2)/3], green[2][(k-ROW2)/3]), k, 2); // Stops after index 136
   }
   for (int l = ROW3; l < 188; l+=3) {
     strip.fill(strip.Color(red[3][(l-ROW3)/3], blue[3][(l-ROW3)/3], green[3][(l-ROW3)/3]), l, 2); // Stops after index 186
   }
-  for (int m = ROW4; m < 238; m+=3) {
+  for (int m = 236; m >= ROW4; m-=3) {
     strip.fill(strip.Color(red[4][(m-ROW4)/3], blue[4][(m-ROW4)/3], green[4][(m-ROW4)/3]), m, 2); // Stops after index 236
   }
   for (int n = ROW5; n < 288; n+=3) {
